8d8b2bc7d785c7a5ff9cd457b8eeb59ee5818fde
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-11-03 16:10:51 -0300

    Added ball calling and ball history;

---
+++ Assets/Script/BallHistory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHistory : MonoBehaviour
+{
+    [SerializeField] Ball ballPrefab;
+    [SerializeField] int ballCount = 5;
+    [SerializeField] float padding = 100;
+
+    Ball[] balls = { };
+    List<int> calledNumbers = new List<int>();
+
+    private void Awake()
+    {
+        SpawnBalls();
+        Game.OnBallCalled += AddNumber;
+    }
+
+    private void SpawnBalls()
+    {
+        balls = new Ball[ballCount];
+        for (int i = 0; i < ballCount; i++)
+        {
+            balls[i] = Instantiate(ballPrefab, transform);
+            balls[i].transform.localPosition = new Vector2 {
+                x = i * padding,
+                y = 0
+            };
+            balls[i].gameObject.SetActive(false);
+
+            if (i > 0)
+                balls[i].transform.localScale *= .75f;
+        }
+    }
+
+    private void AddNumber(int number)
+    {
+        calledNumbers.Add(number);
+        UpdateBalls();
+    }
+
+    private void UpdateBalls()
+    {
+        for (int i = 0; i < balls.Length && i < calledNumbers.Count; i++)
+        {
+            balls[i].gameObject.SetActive(true);
+            balls[i].SetBall(calledNumbers[calledNumbers.Count - 1 - i]);
+        }
+    }
+}
---
+++ Assets/Script/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Game : MonoBehaviour
+{
+    public static event Action OnGameStart;
+    public static event Action OnGameEnd;
+    public static event Action<int> OnBallCalled;
+
+    [SerializeField] float ballDelay = 5;
+
+    protected void Awake ()
+	{
+        OnGameStart?.Invoke();
+        StartCoroutine(CallBalls());
+    }
+
+    private IEnumerator CallBalls()
+    {
+        var wait = new WaitForSeconds(ballDelay);
+
+        while (enabled)
+        {
+            yield return wait;
+            OnBallCalled?.Invoke(Random.Range(1, 76));
+        }
+    }
+}
